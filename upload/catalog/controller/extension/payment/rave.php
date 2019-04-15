3f479df405db4ca2fdff02250f6a71d910bf7fd9
anjolabassey <anjolabassey@yahoo.com>
2019-04-15 12:21:43 +0100

    update to use same base url

--- upload/catalog/controller/extension/payment/rave.php
+++ upload/catalog/controller/extension/payment/rave.php
@@ -1,10 +1,11 @@
 <?php
-class ControllerExtensionPaymentRave extends Controller {
+class ControllerExtensionPaymentRave extends Controller
+{
     public function index()
     {
         $this->language->load('extension/payment/rave');
 
-        
+
         $data['text_testmode'] = $this->language->get('text_testmode');
         $data['button_confirm'] = $this->language->get('button_confirm');
 
@@ -15,10 +16,9 @@ class ControllerExtensionPaymentRave extends Controller {
             $data['script_tag'] = 'https://api.ravepay.co/flwv3-pug/getpaidx/api/flwpbf-inline.js';
         } else {
             $data['public_key'] = $this->config->get('payment_rave_test_public_key');
-            $data['script_tag'] = 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/flwpbf-inline.js';
-
+            $data['script_tag'] = 'https://api.ravepay.co/flwv3-pug/getpaidx/api/flwpbf-inline.js';
         }
-        
+
         $this->load->model('checkout/order');
 
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
@@ -26,7 +26,7 @@ class ControllerExtensionPaymentRave extends Controller {
         if ($order_info) {
 
             $data['reference'] = uniqid('' . $this->session->data['order_id'] . '-');
-            $data['amount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);/// * 100;
+            $data['amount'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false); /// * 100;
             $data['email'] = $order_info['email'];
             $data['firstname'] = $order_info['firstname'];
             $data['lastname'] = $order_info['lastname'];
@@ -41,6 +41,9 @@ class ControllerExtensionPaymentRave extends Controller {
                 case 'ZAR':
                     $country = 'ZA';
                     break;
+                case 'TZS':
+                    $country = 'TZ';
+                    break;
                 default:
                     $country = 'NG';
                     break;
@@ -51,33 +54,31 @@ class ControllerExtensionPaymentRave extends Controller {
             $data['modal_desc'] = $this->config->get('payment_rave_modal_desc');
             $data['callback_url'] = $this->url->link('extension/payment/rave/callback', 'reference=' . rawurlencode($data['reference']), 'SSL');
             return $this->load->view('extension/payment/rave', $data);
-
         }
     }
 
     protected function verify_payment($reference)
     {
         if ($this->config->get('payment_rave_live')) {
-            $url =  'https://api.ravepay.co/flwv3-pug/getpaidx/api/verify';
+            
             $secret_key = $this->config->get('payment_rave_live_secret_key');
-            
         } else {
-            $url =  'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/verify';
-            $secret_key = $this->config->get('payment_rave_test_secret_key');
             
+            $secret_key = $this->config->get('payment_rave_test_secret_key');
         }
-        
+
+        $url =  'https://api.ravepay.co/flwv3-pug/getpaidx/api/verify';
         $response = [];
         $postdata = array(
             'flw_ref' => $reference,
             'SECKEY' => $secret_key,
-          'sslverify' => false
+            'sslverify' => false
         );
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postdata));                                              
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postdata));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
@@ -99,19 +100,19 @@ class ControllerExtensionPaymentRave extends Controller {
         if (isset($this->request->get['reference']) && isset($this->request->get['flw_reference'])) {
             $reference = $this->request->get['reference'];
             $flw_reference = $this->request->get['flw_reference'];
-           
-            
+
+
             // order id is what comes before the first dash in trxref
             $order_id = substr($reference, 0, strpos($reference, '-'));
             // if no dash were in transation reference, we will have an empty order_id
-            if(!$order_id) {
+            if (!$order_id) {
                 $order_id = 0;
             }
-        
+
             $this->load->model('checkout/order');
 
             $order_info = $this->model_checkout_order->getOrder($order_id);
-            
+
             if ($order_info) {
 
                 if ($this->config->get('rave_debug')) {
@@ -119,27 +120,25 @@ class ControllerExtensionPaymentRave extends Controller {
                 }
                 $amount = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false);
                 $result = $this->verify_payment($flw_reference);
-                
+
                 $order_status_id = $this->config->get('payment_config_order_status_id');
                 if ($result->data->flwMeta->chargeResponse === '00' || $result->data->flwMeta->chargeResponse === '0') {
-                    if($amount ==  $result->data->amount){
+                    if ($amount ==  $result->data->amount) {
                         $order_status_id = $this->config->get('payment_rave_approved_status_id');
                         $redir_url = $this->url->link('checkout/success');
-                    }else{
+                    } else {
                         $order_status_id = $this->config->get('payment_rave_error_status_id');
                         $redir_url = $this->url->link('checkout/checkout', 'Invalid amount paid', 'SSL');
                     }
-                    
                 } else {
                     $order_status_id = $this->config->get('payment_rave_error_status_id');
                     $redir_url = $this->url->link('checkout/checkout', '', 'SSL');
                 }
- 
-                $this->model_checkout_order->addOrderHistory($order_id, $order_status_id, "Transaction reference: ".$reference, true);
-                
+
+                $this->model_checkout_order->addOrderHistory($order_id, $order_status_id, "Transaction reference: " . $reference, true);
+
                 $this->response->redirect($redir_url);
             }
         }
     }
-
 }
